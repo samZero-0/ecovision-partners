79d48e66572f0f41435603902423888073a1b788
Samin <samineminel271@gmail.com>
2025-04-03 22:11:33 +0600

    firebase api updated

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,18 +1,15 @@
-
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDGhRaXDWGe5d9n6KeZlHNkfuN58KRq0gQ",
-  authDomain: "job-portal-b60db.firebaseapp.com",
-  projectId: "job-portal-b60db",
-  storageBucket: "job-portal-b60db.firebasestorage.app",
-  messagingSenderId: "575516681600",
-  appId: "1:575516681600:web:74c5c01cee20ba0e384d65"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
